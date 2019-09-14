3ff8b2492bc4a75346ceab9e1fab95c0a7258934
Garrett Cornwell <garrett.w.d.cornwell@gmail.com>
2019-09-14 11:04:14 -0600

    Update install_prereq.sh

--- install_prereq.sh
+++ install_prereq.sh
@@ -3,7 +3,11 @@
 set -e
 
 sudo apt-get update
-sudo apt-get upgrade -y
+
+# Don't need to upgrade everything
+# Used kali image from here as VM and passedthrough USB TP-LINK TL-WN722N: https://cdimage.kali.org/kali-2018.4/kali-linux-2018.4-amd64.iso
+# sudo apt-get upgrade -y
+
 sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel mosquitto haveged net-tools
 
 sudo pip3 install paho-mqtt pyaes tornado
